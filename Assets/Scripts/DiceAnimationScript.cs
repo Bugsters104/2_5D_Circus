b2b3bf910a2e1142615dcaedeedd0ed8a54c768a
Bugsters104 <r.kristaps@inbox.lv>
2025-03-07 14:57:11 +0200

    End of second lesson

---
+++ Assets/Scripts/DiceAnimationScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceAnimationScript : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void RollDice() {
+        animator.SetBool("isRolling", true);
+    }
+
+    public void StopDiceRoll() {
+        animator.SetBool("isRolling", false);
+    }
+}
